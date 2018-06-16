8481a1e751da460232b7d5cc3d1b94632baad400
sarithapolisetty <sarithapolisetty14@gmail.com>
2018-06-15 20:01:26 -0700

    Fix grid generation for profer size

--- TurtleGraphics.js
+++ TurtleGraphics.js
@@ -1,147 +1,118 @@
+class Turtle {
+    constructor(x, y) {
+        this.loc = [x, y];
+        this.allPointsArr = [[x, y]];
+        this.gridSize = (x > y) ? x : y;
+        /*
+         0 will point to east, 1 will point to south, 2 will point to west
+         and 3 will point to north as per the right angle directions.
+        */
+        this.currDir = 0;
+    }
 
-class Turtle 
-{
-    constructor(x,y) 
-    {
-        this.x = x;
-        this.y = y;
-        this.steps = [[x,y]];
-        this.facing = 1;  
-    } 
-    forward(nSteps)
-    {
-        for(let i = 0; i < nSteps; i += 1)
-        {
-            this.position();
+    /*
+    This function takes no. of steps the turtle will move forward.
+    Depending on the current direction, the path traversed by the
+    turtle will be added to the allPointsArr.
+    */
+    forward(steps) {
+        for (let row = 1; row <= steps; row++) {
+            if (this.currDir === 0) {
+                this.loc[0] = this.loc[0] + 1;
+            }
+            else if (this.currDir === 1) {
+                this.loc[1] = this.loc[1] + 1;
+            }
+            else if (this.currDir === 2) {
+                this.loc[0] = this.loc[0] - 1;
+            }
+            else {
+                this.loc[1] = this.loc[1] - 1;
+            }
+            this.gridSize = (this.loc[0] > this.gridSize) ? this.loc[0] : this.gridSize;
+            this.gridSize = (this.loc[1] > this.gridSize) ? this.loc[1] : this.gridSize;
+
+            this.allPointsArr.push([this.loc[0], this.loc[1]]);
         }
         return this;
     }
-    position() 
-    {
-        switch(this.facing)
-        {
-            case 0 :
-            this.steps.push([this.x,this.y]);
-            this.y --;
-            break;
-            case 1 :
-            this.steps.push([this.x,this.y]);
-            this.x ++;
-            break;
-            case 2 :
-            this.steps.push([this.x,this.y]);
-            this.y ++;
-            break;
-            case 3 :
-            this.steps.push([this.x,this.y]);
-            this.x --;
-            break;
-        }
-    }
-    right()
-    {
-        this.facing = (this.facing + 1) % 4;
-        return this;    
-    }
-    left()
-    {
-        this.facing = (this.facing + 3) % 4;
-        return this;   
-    }
-    allPoints()
-    {
-        return this.steps;    
-    }
-    print()
-    {
-        for (let i = this.startX(); i <= this.endX() + 1; i++) 
-        {
-            let res = '';
-            for (let j = this.startY(); j <= this.endY() + 1; j++) 
-            {
-                if (this.isSteps(j, i)) 
-                {
-                    res += "●";
-                }
-                else 
-                {
-                    res += " ";
+
+    //This function prints the grid and the path traversed by the turtle.
+    print() {
+        let result = "";
+        for (let row = 0; row <= this.gridSize; row++) {
+            for (let col = 0; col <= this.gridSize; col++) {
+                if (this.doesLocExist([col, row])) {
+                    result += "●";
+                } else {
+                    result += " ";
                 }
             }
-            console.log(res);
+            result += "\n";
         }
+        console.log(result);
     }
-    startX() 
-    {
-        let start = 0;
-        for (let i = 0; i < this.steps.length; i++) 
-        {
-            if (this.steps[i][0] < start) 
-            {
-                start = this.steps[i][0];
-            }
-        }
-        return start;
+
+    /*
+    This function checks whether the given location (x,y) is part of the
+    path traversed by the turtle.
+    */
+    doesLocExist(loc) {
+        return (this.allPointsArr.some(element =>
+            (element[0] === loc[0] && element[1] === loc[1])));
     }
-    startY() 
-    {
-        let start = 0;
-        for (let i = 0; i < this.steps.length; i++) 
-        {
-            if (this.steps[i][1] < start) 
-            {
-                start = this.steps[i][1];
-            }
+
+    /*
+    This function changes the current direction of the turtle when it turns
+    right.
+    */
+    right() {
+
+        if (this.currDir === 3) {
+            this.currDir = 0;
+        } else {
+            this.currDir++;
         }
-        return start;
+        return this;
     }
-    endX() 
-    {
-        let end = this.steps[0][0];
-        for (let i = 1; i < this.steps.length; i++) 
-        {
-            if (this.steps[i][0] > end) 
-            {
-                end = this.steps[i][0];
-            }
+
+    /*
+    This function changes the current direction of the turtle when it turns
+    left.
+    */
+    left() {
+
+        if (this.currDir === 0) {
+            this.currDir = 3;
+        } else {
+            this.currDir--;
         }
-        return end;
+        return this;
     }
-    endY() 
-    {
-        let end = this.steps[0][1];
-        for (let i = 1; i < this.steps.length; i++) 
-        {
-            if (this.steps[i][1] > end) 
-            {
-                end = this.steps[i][1];
-            }
-        }
-        return end;
+
+    /*
+    This function returns the array containing the locations traversed by the
+    turtle.
+    */
+    allPoints() {
+        return this.allPointsArr;
     }
-    isSteps(x, y) 
-    {
-        for (let i = 0; i < this.steps.length; i++) 
-        {
-            if (this.steps[i][0] == x && this.steps[i][1] == y) 
-            {
-                return true;
-            }
-        }
-        return false;
-    }       
+
 }
 
-console.log(new Turtle(0,0)
-.forward(5)
-.right()
-.forward(5)
-.right()
-.forward(5)
-.right()
-.forward(5)
-.print());
-
-
-    
+const flash = new Turtle(0, 4)
+    .forward(5)
+    .left()
+    .forward(3)
+    .right()
+    .forward(5)
+    .right()
+    .forward(8)
+    .right()
+    .forward(5)
+    .right()
+    .forward(3)
+    .left()
+    .forward(3)
+    .print();
 
