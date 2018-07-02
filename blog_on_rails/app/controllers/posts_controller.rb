ffe35729fafd78738a4ef9c78ef417a4d91af875
sarithapolisetty <sarithapolisetty14@gmail.com>
2018-07-02 13:36:04 -0700

    Applied all CRUD actions on Posts controller

---
+++ blog_on_rails/app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new post_params
+        if @post.save
+            redirect_to @post
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def index
+        @posts = Post.order(created_at: :DESC)
+    end
+
+    def destroy
+        @post.destroy
+        redirect_to posts_path
+    end
+
+    def edit
+    end
+
+    def update
+        if @post.update post_params
+           redirect_to post_path(@post)
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+
+    def find_post
+        @post = Post.find params[:id]
+    end
+end
